7cce755c8a739514c12ffa474638d0b45b28d6fc
ExecutWBXrn <executwbxrn@gmail.com>
2023-12-17 13:04:26 +0000

    serve data into templates

--- steamcat/views.py
+++ steamcat/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
 def index(request):
-    return render(request, "steamcat/index.html")
+    context = {
+        "title":"steamofficial.com",
+    }
+    return render(request, "steamcat/index.html", context=context)
 
 def about(request):
-    return render(request, "steamcat/about.html")
+    context={
+        "title":"steamofficialaboutpage.com",
+    }
+    return render(request, "steamcat/about.html", context=context)
 
 def notfound(request, exception):
     return HttpResponseNotFound("No hello steam(")
