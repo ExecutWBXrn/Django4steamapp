5f664bbe499c80350796e09a6eaf7384fb1278fc
ExecutWBXrn <executwbxrn@gmail.com>
2023-12-22 21:59:34 +0000

    add models

--- steamcat/models.py
+++ steamcat/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Games(models.Model):
+    game = models.CharField(max_length=255)
+    year = models.IntegerField(blank=True)
+    describe = models.TextField(blank=True)
+    price = models.IntegerField()
+    url = models.TextField(blank=True)
+    is_published = models.BooleanField(default=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+w = Games(game = "ELDEN RING", year = 2022, describe = "THE NEW FANTASY ACTION RPG. Rise, Tarnished, and be guided by grace to brandish the power of the Elden Ring and become an Elden Lord in the Lands Between.", price = 1799, url = "https://store.steampowered.com/app/1245620/ELDEN_RING/")
